665a084d0083767ee9e041b55fbce8be4dc5cfad
Niharika-1912 <54639575+Niharika-1912@users.noreply.github.com>
2019-09-17 02:47:42 -0400

    Assignment 3

---
+++ CoreCrud/Pages/ManufacturerProfile.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CoreCrud.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreCrud.Pages
+{
+    public class ManufacturerProfileModel : PageModel
+    {
+
+        private CoreCrudContext _context;
+        public ManufacturerProfileModel(CoreCrudContext context)
+        {
+            _context = context;
+        }
+
+        public Manufacturer Manufacturer { get; set; }
+        public void OnGet(int? id)
+        {
+
+            Manufacturer = _context.Manufacturer.Include(i=>i.Watches).FirstOrDefault(i=>i.ID==id);
+
+        }
+    }
+}
+
